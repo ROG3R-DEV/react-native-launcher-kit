d128ed76e815ad719b06f29eb0de989fe31b1ad0
Rogério Luiz <rogerio.macdev@gmail.com>
2023-05-08 19:02:41 -0300

    🐛 Add ReadableMap

--- android/src/main/java/com/launcherkit/LauncherKitModule.java
+++ android/src/main/java/com/launcherkit/LauncherKitModule.java
@@ -3,6 +3,7 @@ package com.launcherkit;
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
